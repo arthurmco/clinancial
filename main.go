1bff63b80b6790427094a33737edb0b824a46239
Arthur Mendes <arthurmco@gmail.com>
2017-10-05 00:02:17 -0300

    Add skeleton command line interface

--- main.go
+++ main.go
@@ -9,11 +9,63 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
-func main() {
-	f := FinancialRegister{name: "Comprei Pão", value: 38.20};
-	
-	fmt.Printf("Hello world (%s => %.2f)\n", f.name, f.value);
+type CCommandFunc func([]string)
 
+type CCommand struct {
+	name string
+	desc string
+	function CCommandFunc	
+}
+
+var commands = make([]CCommand, 0);
+
+func printHelp() {
+	fmt.Println(" clinancial - a command-line financial manager");
+	fmt.Println("");
+	fmt.Println(" Commands: ");
+
+	for _, c := range(commands) {
+		fmt.Printf("\t%-20s %s\n", c.name, c.desc);
+	}
+}
+
+func main() {
+
+	commands = append(commands,
+		CCommand{name: "help", desc: "Print this help text",
+			function: _printHelp},
+		CCommand{name: "argprint", desc: "Test argument printing",
+			function: testArgs});
+		
+
+	// Check command
+	if len(os.Args) <= 1 {
+		printHelp();
+		return;
+	}
+
+	for _, c := range(commands) {
+		if c.name == os.Args[1] {
+			c.function(os.Args[2:]);
+			return;
+		}
+	}
+
+	fmt.Println("No command named "+ os.Args[1]);
+}
+
+func _printHelp(args []string) {
+	printHelp();
+}
+
+func testArgs(args []string) {
+	fmt.Println("");
+	for _, s := range args {
+		fmt.Print(s, " - ");
+	}
+
+	fmt.Println("");
 }
