ff35b9efc22d474d7b8435942a0fa34a413ecf30
Arthur Mendes <arthurmco@gmail.com>
2017-10-08 23:32:57 -0300

    Add basic financial register creation
    Currently it only creates registers for valid accounts, and sets the
    date to the actual date.

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"fmt"
 	"os"
 	"time"
+	"strings"
+	"bufio"
 )
 
 type CCommandFunc func([]string)
@@ -46,6 +48,9 @@ func main() {
 			function: _printHelp},
 		CCommand{name: "account", desc: "Manages accounts",
 			function: manageAccounts},
+		CCommand{name: "register",
+			desc: "Manages financial registers, i.e transactions",
+			function: manageRegisters},
 		CCommand{name: "argprint", desc: "Test argument printing",
 			function: testArgs})
 
@@ -79,6 +84,149 @@ func testArgs(args []string) {
 	fmt.Println("")
 }
 
+
+func manageRegisters(args []string) {
+	if len(args) < 2 {
+		fmt.Println("Expected format: " + args[0] + " [create|view]")
+		return
+	}
+
+	operation := args[1]
+	
+	if operation == "create" {
+		var acname string
+		var acval float32
+		var acfrom, acto *Account
+		accready := false
+
+		accounts, aerr := GetAllAccounts()
+		if aerr != nil {
+			panic(aerr)
+		}
+
+		if len(accounts) == 0 {
+			panic("No account created. \n"+
+				"Please type "+os.Args[0]+" account create <acc> to create an account (named <acc>)")
+
+			
+		}
+
+		accstrlist := make([]string, 0)
+		for _, aval := range accounts {
+			astr := fmt.Sprintf("%d: %s",
+				aval.GetID(), aval.GetName())
+
+			accstrlist = append(accstrlist, astr)
+		}
+
+		for !accready {
+			// Request register name
+			fmt.Print("Name: ")
+			rd := bufio.NewReader(os.Stdin)
+			
+			acname, err := rd.ReadString('\n')
+			acname = acname[0:len(acname)-1]
+			var num int = 0
+
+			if err != nil {
+				panic(err)
+			}
+
+			// Request value
+			valready := false
+			for !valready {
+				fmt.Print("Value ($): ")
+				num, err = fmt.Scanf("%f", &acval)
+
+				if num <= 0 {
+					fmt.Fprint(os.Stderr,
+						"Invalid price format")
+				}
+
+				if err != nil {
+					panic(err)
+				}
+				valready = true
+			}
+
+			// Request src account
+			var fromacc int
+			fromready := false
+			for !fromready {
+				fmt.Println("Choose the origin account (the one to be debited)")
+				fmt.Println("Available ones: " + strings.Join(accstrlist, ", "))
+				fmt.Print("Number: ")
+				num, err = fmt.Scanf("%d", &fromacc)
+
+				for _, acc := range accounts {
+					if acc.GetID() == uint(fromacc) {
+						acfrom = acc
+						fromready = true
+						break
+					}
+				}
+
+				if !fromready {
+					fmt.Fprintf(os.Stderr,
+						"This account does not exist")
+					
+				}
+			}
+
+
+			// Request dest account
+			var toacc int
+			toready := false
+			for !toready {
+				fmt.Println("Choose the destiny account (the one to be credited)")
+				fmt.Println("Available ones: " + strings.Join(accstrlist, ", "))
+				fmt.Print("Number: ")
+				num, err = fmt.Scanf("%d", &toacc)
+
+				for _, acc := range accounts {
+					if acc.GetID() == uint(toacc) {
+						acto = acc
+						toready = true
+						break
+					}
+				}
+
+				if !toready {
+					fmt.Fprintf(os.Stderr,
+						"This account does not exist")
+					
+				}
+			}
+
+
+			strfrom := acfrom.GetName()
+			strto := acto.GetName()
+			fmt.Printf("Creating register '%s' with value %.2f, from account %s to account %s"+
+				"\n\tConfirm (Y/N) or Ctrl+C to exit\n", acname, acval, strfrom, strto)
+
+			res := "N"
+			fmt.Scanf("%s", &res)
+
+				
+			if res == "Y" || res == "y" {
+				accready = true
+			}
+		}
+		
+		freg := &FinancialRegister{name: acname, value: acval,
+			from: acfrom, to: acto, time: time.Now()}
+		err := acfrom.AddRegister(freg)
+
+		if err != nil {
+			panic(err)
+		}
+		
+		
+	}
+
+}
+
+
 func manageAccounts(args []string) {
 	if len(args) < 2 {
 		fmt.Println("Expected format: " + args[0] + " [create|view|delete]")
@@ -93,7 +241,7 @@ func manageAccounts(args []string) {
 			return
 		}
 
-		acc_name := args[2]
+		acc_name := strings.TrimSpace(args[2])
 		a := &Account{id: uint(time.Now().Unix()),
 			name: acc_name}
 		a.Create()
